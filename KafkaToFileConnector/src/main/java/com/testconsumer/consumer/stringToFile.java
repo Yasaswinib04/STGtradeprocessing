de329b2b2afa43427d5d84450b428e3f62839e1a
Avichal <avichal.rang@gmail.com>
2020-06-07 20:51:04 +0530

    Added unit testing

--- KafkaToFileConnector/src/main/java/com/testconsumer/consumer/stringToFile.java
+++ KafkaToFileConnector/src/main/java/com/testconsumer/consumer/stringToFile.java
@@ -1,17 +1,5 @@
 package com.testconsumer.consumer;
 
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.StringReader;
 
 public class stringToFile extends Thread{
     private String xmlString;
@@ -24,16 +12,8 @@ public class stringToFile extends Thread{
 
     @Override
     public void run() {
-            convert(xmlString,i);
+            new Converter().convert(xmlString,i);
     }
 
-    public void convert(String xmlString,int i){
-        try {
-            FileWriter fileWriter=new FileWriter("Output Trades/Trade"+Integer.toString(i)+".xml");
-            fileWriter.write(xmlString);
-            fileWriter.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+
 }
--- KafkaToFileConnector/src/test/java/com/testconsumer/consumer/ConsumerApplicationTests.java
+++ KafkaToFileConnector/src/test/java/com/testconsumer/consumer/ConsumerApplicationTests.java
@@ -1,13 +1,39 @@
 package com.testconsumer.consumer;
 
+import org.apache.commons.io.FileUtils;
+import org.assertj.core.api.FileAssert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Convert;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.File;
+import java.io.IOException;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 class ConsumerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    String s="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<Trades>\n" +
+            "\t<Trade>\n" +
+            "\t\t\t\t\t<TradeId>HNC234712121212</TradeId>\n" +
+            "\t\t\t\t\t<Firm>SLI</Firm>\n" +
+            "\t\t\t\t\t<CashSecurity>\n" +
+            "\t\t\t\t\t\t\t<SecurityType>EQ</SecurityType>\n" +
+            "\t\t\t\t\t\t\t<SecurityIdentifier>XX4567</SecurityIdentifier>\n" +
+            "\t\t\t\t\t</CashSecurity>\n" +
+            "\t\t\t\t\t<TradeDate>2020-05-13</TradeDate>\n" +
+            "\t</Trade>\n" +
+            "</Trades>";
+    File f=new File("src/main/resources/Trade1.xml");
+    @Test
+    public void testCase() throws IOException {
+        Converter converter=new Converter();
+        converter.convert(s,1);
+        File f1=new File("Output Trades/Trade1.xml");
+        f1.deleteOnExit();
+        assertTrue(FileUtils.contentEquals(f,f1));
+    }
 }
