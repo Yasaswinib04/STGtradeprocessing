3f8e869e68da8818b6b4d5eca325003f1e9e0510
GoluKumar4024 <52768804+GoluKumar4024@users.noreply.github.com>
2020-06-17 16:07:11 +0530

    Add files via upload

---
+++ RuleEngineInJava/src/test/java/com/example/RuleEngineTest/RuleEngineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RuleEngineTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
